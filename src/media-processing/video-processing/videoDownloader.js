0d4b45e929b2eefd38d2caf75984f402358e9bfd
Derah <55749489+Dev-derah@users.noreply.github.com>
2025-04-06 14:37:00 +0100

    update: add proxy to ytdl download options

--- src/media-processing/video-processing/videoDownloader.js
+++ src/media-processing/video-processing/videoDownloader.js
@@ -317,6 +317,7 @@ class VideoDownloader {
               "referer:youtube.com",
               "user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             ],
+            proxy: process.env.YTDL_PROXY,
           };
 
           if (cookiePath) {
